b21ad8357887fe21649349658f9a25492636c99c
Dusan <dusanvulic11@gmail.com>
2022-08-22 12:22:05 +0200

    express server created

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+
+//express app
+const app = express();
+
+// routes
+app.get("/", (req, res) => {
+    res.json({ msg: "welcome to the app" });
+});
+
+// listen for requests
+
+app.listen(process.env.PORT, () => {
+    console.log(`server listening on port ${process.env.PORT})`);
+});
