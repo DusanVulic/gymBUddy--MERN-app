4527d9452ff6c39ab2f734055447bab2cc59333e
Dusan <dusanvulic11@gmail.com>
2022-08-22 19:36:04 +0200

    models and schemas

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+//import model
+
+const Workout = require("../models/workoutModel");
+
 const router = express.Router();
 
 //GET ALL WORKOUTS
@@ -13,8 +17,17 @@ router.get("/:id", (req, res) => {
 });
 
 //POST (create ) NEW WORKOUT
-router.post("/", (req, res) => {
-    res.json({ msg: "post / create new workout" });
+router.post("/", async(req, res) => {
+    const { title, load, reps } = req.body;
+
+    try {
+        const workout = await Workout.create({ title, load, reps });
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+
+    //res.json({ msg: "post / create new workout" });
 });
 
 //DELETE WORKOUT
