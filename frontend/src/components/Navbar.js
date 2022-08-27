f28cfdd8872a9aa236a88467d17d0452b1fe1ffe
Dusan <dusanvulic11@gmail.com>
2022-08-27 10:55:48 +0200

    setting initial AUTH

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,8 +3,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 
+import { useAuthContext } from "../hooks/useAuthContext";
+
 const Navbar = () => {
   const { logout } = useLogout();
+
+  const { user } = useAuthContext();
 
   const logoutHandler = () => {
     logout();
@@ -17,13 +21,17 @@ const Navbar = () => {
           <h1>Workout body</h1>
         </Link>
         <nav>
-          <div>
-            <button onClick={logoutHandler}>logout</button>
-          </div>
-          <div>
-            <Link to="/login">login</Link>
-            <Link to="/signup">signup</Link>
-          </div>
+          {user && (
+            <div>
+              <button onClick={logoutHandler}>logout</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">login</Link>
+              <Link to="/signup">signup</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
